f143f52dd96f4dca27cc476e0d552108c0d4f618
espoir <yghack@gmail.com>
2024-08-15 15:51:47 +0800

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,16 +1,18 @@
+const homeSsid = "NETGEAR24-5G"; //你可以换成你自己的 WIFI 名
+
 function handleNetworkChange() {
   let ssid = $network.wifi.ssid;
   let cellularType = $network.v4.primaryInterface;
 
   if (ssid === homeSsid) {
     $surge.setOutboundMode("direct");
-    // $notification.post("Surge 出站模式已更改", "", "已切换至直接连接模式（HOME_WiFi）");
+    $notification.post("Surge 出站模式已更改", "", "已切换至直接连接模式（家庭WiFi）");
   } else if (ssid) {
     $surge.setOutboundMode("rule");
-    // $notification.post("Surge 出站模式已更改", "", "已切换至规则模式（其他WiFi）");
+    $notification.post("Surge 出站模式已更改", "", "已切换至规则模式（其他WiFi）");
   } else if (cellularType && cellularType.startsWith("pdp_ip")) {
     $surge.setOutboundMode("direct");
-    // $notification.post("Surge 出站模式已更改", "", "已切换至规则模式（蜂窝数据）");
+    $notification.post("Surge 出站模式已更改", "", "已切换至规则模式（蜂窝数据）");
   } else {
     $notification.post("网络状态", "", "无网络连接");
   }
